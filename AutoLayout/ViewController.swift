7936bdb51a2ea7471c1b6acc93e328bbd7df8edc
nokonoko <nokonoko2057@gmail.com>
2023-04-21 14:28:31 +0900

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by yuki takei on 2023/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
